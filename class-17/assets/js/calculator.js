e0d956ab87f22561cd9c820a89621832fbac2746
azharanowar <azharanowar@gmail.com>
2022-06-22 11:10:06 +0600

    Basic calculator functionalities change.

--- class-17/assets/js/calculator.js
+++ class-17/assets/js/calculator.js
@@ -36,55 +36,121 @@
 
 
 
-var addition = document.getElementById('addition');
-addition.onclick = function() {
-    calculator('+');
+
+
+
+// function calculator( operator ) {
+//     var firstNumber = Number( document.getElementById('firstNumber').value );
+//     var secondNumber = Number( document.getElementById('secondNumber').value );
+//
+//     switch ( operator ) {
+//         case '+' :
+//             document.getElementById('result').value = firstNumber + secondNumber;
+//             break;
+//         case '-' :
+//             document.getElementById('result').value = firstNumber - secondNumber;
+//             break;
+//         case '*' :
+//             document.getElementById('result').value = firstNumber * secondNumber;
+//             break;
+//         case '/' :
+//             document.getElementById('result').value = firstNumber / secondNumber;
+//             break;
+//         case '%' :
+//             document.getElementById('result').value = firstNumber % secondNumber;
+//             break;
+//     }
+//
+//
+//
+// }
+//
+// var addition = document.getElementById('addition');
+// addition.onclick = function() {
+//     calculator('+');
+// }
+//
+// var subtraction = document.getElementById('subtraction');
+// subtraction.onclick = function() {
+//     calculator('-');
+// }
+//
+// var multiplication = document.getElementById('multiplication');
+// multiplication.onclick = function() {
+//     calculator('*');
+// }
+//
+// var division = document.getElementById('division');
+// division.onclick = function() {
+//     calculator('-');
+// }
+//
+// var reminder = document.getElementById('reminder');
+// reminder.onclick = function() {
+//     calculator('%');
+// }
+
+
+
+
+
+var equalBtn = document.getElementById('equalBtn');
+equalBtn.onclick = function () {
+    var operator = document.getElementById('operator').value;
+    var result = myCalculator( operator );
+    document.getElementById('result').value = result;
 }
 
-var subtraction = document.getElementById('subtraction');
-subtraction.onclick = function() {
-    calculator('-');
-}
-
-var multiplication = document.getElementById('multiplication');
-multiplication.onclick = function() {
-    calculator('*');
-}
-
-var division = document.getElementById('division');
-division.onclick = function() {
-    calculator('-');
-}
-
-var reminder = document.getElementById('reminder');
-reminder.onclick = function() {
-    calculator('%');
-}
-
-
-function calculator( operator ) {
+function myCalculator( operator ) {
     var firstNumber = Number( document.getElementById('firstNumber').value );
     var secondNumber = Number( document.getElementById('secondNumber').value );
-
-    switch ( operator ) {
+    switch (operator) {
         case '+' :
-            document.getElementById('result').value = firstNumber + secondNumber;
+            var result = firstNumber + secondNumber;
             break;
         case '-' :
-            document.getElementById('result').value = firstNumber - secondNumber;
+            var result = firstNumber - secondNumber;
             break;
         case '*' :
-            document.getElementById('result').value = firstNumber * secondNumber;
+            var result = firstNumber * secondNumber;
             break;
         case '/' :
-            document.getElementById('result').value = firstNumber / secondNumber;
+            var result = firstNumber / secondNumber;
             break;
         case '%' :
-            document.getElementById('result').value = firstNumber % secondNumber;
+            var result = firstNumber % secondNumber;
             break;
     }
 
-
-
+    return result;
 }
+
+var  addition = document.getElementById('addition');
+addition.onclick = function () {
+    document.getElementById('operator').value = '+';
+}
+
+var subtraction = document.getElementById('subtraction');
+subtraction.onclick = function () {
+    document.getElementById('operator').value = '-';
+}
+
+var multiplication = document.getElementById('multiplication');
+multiplication.onclick = function () {
+    document.getElementById('operator').value = '*';
+}
+
+var division = document.getElementById('division');
+division.onclick = function () {
+    document.getElementById('operator').value = '/';
+}
+
+var reminder = document.getElementById('reminder');
+reminder.onclick = function () {
+    document.getElementById('operator').value = '%';
+}
+
+
+
+
 
