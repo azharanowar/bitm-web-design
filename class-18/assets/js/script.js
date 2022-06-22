7a5351992eb488404b9b4128bf590811612aeed8
azharanowar <azharanowar@gmail.com>
2022-06-22 22:03:25 +0600

    Another function added that changing contents by clicking different buttons.

--- class-18/assets/js/script.js
+++ class-18/assets/js/script.js
@@ -35,38 +35,54 @@
 
 
 
-var defaultBtn = document.getElementById('defaultBtn');
-defaultBtn.onclick = function () {
-    var divOne = document.getElementById('divOne');
-    divOne.className = 'div-one';
+// var defaultBtn = document.getElementById('defaultBtn');
+// defaultBtn.onclick = function () {
+//     var divOne = document.getElementById('divOne');
+//     divOne.className = 'div-one';
+// }
+//
+// var redBtn = document.getElementById('redBtn');
+// redBtn.onclick = function () {
+//     var divOne = document.getElementById('divOne');
+//     divOne.className = 'div-one div-one-background-red';
+// }
+//
+// var greenBtn = document.getElementById('greenBtn');
+// greenBtn.onclick = function () {
+//     var divOne = document.getElementById('divOne');
+//     divOne.className = 'div-one div-one-background-green';
+// }
+//
+// var blueBtn = document.getElementById('blueBtn');
+// blueBtn.onclick = function () {
+//     var divOne = document.getElementById('divOne');
+//     divOne.className = 'div-one div-one-background-blue';
+// }
+//
+// var orangeBtn = document.getElementById('orangeBtn');
+// orangeBtn.onclick = function () {
+//     var divOne = document.getElementById('divOne');
+//     divOne.className = 'div-one div-one-background-orange';
+// }
+
+
+var homeBtn = document.getElementById('homeBtn');
+homeBtn.onclick = function () {
+    document.getElementById('heading').innerHTML = "This is heading one from home.";
+    document.getElementById('paragraph').innerHTML = "Home! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa est inventore iste laudantium minima sapiente sequi velit voluptas! Laboriosam, sed!";
 }
 
-var redBtn = document.getElementById('redBtn');
-redBtn.onclick = function () {
-    var divOne = document.getElementById('divOne');
-    divOne.className = 'div-one div-one-background-red';
+var aboutBtn = document.getElementById('aboutBtn');
+aboutBtn.onclick = function () {
+    document.getElementById('heading').innerHTML = "This is heading two from about.";
+    document.getElementById('paragraph').innerHTML = "About! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa est inventore iste laudantium minima sapiente sequi velit voluptas! Laboriosam, sed!";
 }
 
-var greenBtn = document.getElementById('greenBtn');
-greenBtn.onclick = function () {
-    var divOne = document.getElementById('divOne');
-    divOne.className = 'div-one div-one-background-green';
+var contactBtn = document.getElementById('contactBtn');
+contactBtn.onclick = function () {
+    document.getElementById('heading').innerHTML = "This is heading three from contact.";
+    document.getElementById('paragraph').innerHTML = "Contact! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa est inventore iste laudantium minima sapiente sequi velit voluptas! Laboriosam, sed!";
 }
-
-var blueBtn = document.getElementById('blueBtn');
-blueBtn.onclick = function () {
-    var divOne = document.getElementById('divOne');
-    divOne.className = 'div-one div-one-background-blue';
-}
-
-var orangeBtn = document.getElementById('orangeBtn');
-orangeBtn.onclick = function () {
-    var divOne = document.getElementById('divOne');
-    divOne.className = 'div-one div-one-background-orange';
-}
-
-
-
 
 
 
