22c4f16e9c96a86c2509e060d12f81901af118c6
azharanowar <azharanowar@gmail.com>
2022-06-24 22:07:35 +0600

    Basic slider using javascript.

--- class-19/assets/js/script.js
+++ class-19/assets/js/script.js
@@ -1,12 +1,33 @@
-function myFunction() {
-    let dateTime = new Date();
-    let hours = dateTime.getHours();
-    let minutes = dateTime.getMinutes();
-    let seconds = dateTime.getSeconds();
+// function myFunction() {
+//     let dateTime = new Date();
+//     let hours = dateTime.getHours();
+//     let minutes = dateTime.getMinutes();
+//     let seconds = dateTime.getSeconds();
+//
+//
+//     document.getElementById('watch').innerHTML = hours + ':' + minutes + ':' + seconds;
+// }
+//
+// setInterval(myFunction, 1000);
 
 
-    document.getElementById('watch').innerHTML = hours + ':' + minutes + ':' + seconds;
+
+sliderImagesList = document.getElementById('sliderThumbnailList').getElementsByTagName('img');
+
+let sliderImagesLink = [];
+for ( let i = 0; i < sliderImagesList.length; i++ ) {
+    sliderImagesLink[i] = sliderImagesList[i].src;
 }
 
-setInterval(myFunction, 1000);
-// setTimeout();
+
+let i =0;
+setInterval( demo, 2500);
+
+function demo() {
+    // console.log(sliderImagesLink[i]);
+    document.getElementById('mainImg').src = sliderImagesLink[i];
+    i++;
+    if ( i >= sliderImagesLink.length ) {
+        i = 0;
+    }
+}
