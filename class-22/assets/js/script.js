90c402c767dbceb264073b76ae3ae290baa31d30
azharanowar <azharanowar@gmail.com>
2022-06-26 10:51:07 +0600

    Add new function for scrolling event detection and also work for overflow.

--- class-22/assets/js/script.js
+++ class-22/assets/js/script.js
@@ -1,5 +1,47 @@
-$('#submitBtn').click( function () {
-    let firstName = $('#firstName').val();
-    let lastName = $('#lastName').val();
-    $('#fullName').val( firstName + ' ' + lastName );
+// $('#submitBtn').click( function () {
+//     let firstName = $('#firstName').val();
+//     let lastName = $('#lastName').val();
+//     $('#fullName').val( firstName + ' ' + lastName );
+// });
+
+
+
+
+$( '#firstName' ).keyup( function () {
+    let firstName = $( '#firstName' ).val();
+    $( '#result1' ).text( firstName );
 });
+
+$( '#lastName' ).keyup( function () {
+    let lastName = $( '#lastName' ).val();
+    $( '#result2' ).text( lastName );
+});
+
+$( '#lastName' ).blur( function () {
+    let firstName = $( '#firstName' ).val();
+    let lastName = $( '#lastName' ).val();
+    $( '#result3' ).text( firstName + ' ' + lastName );
+});
+
+
+
+
+let firstContentDivScrollPosition = 0;
+$( '#contentDiv' ).scroll( function () {
+    let contentDivScrollPosition = $('#contentDiv').scrollTop();
+
+    if ( contentDivScrollPosition > firstContentDivScrollPosition ) {
+        $( '#overflowResult' ).text('You are scrolling down!');
+    } else {
+        $( '#overflowResult' ).text('You are scrolling Up!');
+    }
+    firstContentDivScrollPosition = contentDivScrollPosition;
+
+});
+
+
+
+
+// Html Element Position Check:
+let overflowResultPosition = $('.output-table').position();
+console.log(overflowResultPosition);
